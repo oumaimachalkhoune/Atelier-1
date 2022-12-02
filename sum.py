ef6a6727b13393336f5a0cc6c7ea9763b2eb45ed
Oumaima Chalkhoune <oumaimachalkhoune1@gmail.com>
2022-12-02 18:46:59 +0100

    sum fonction

---
+++ sum.py
@@ -0,0 +1,10 @@
+#Le calcul de somme des nombres de 1 à n:
+
+def somme(n):
+    s=0
+    for i in range (1,n+1):
+        s=s+i
+    return s
+n=int(input("entrez la valeur de n : "))
+r=somme(n)
+print("la somme de 1 jusqu'à",n,"est",r)
