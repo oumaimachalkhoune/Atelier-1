70c09a9050a22022b87673188b87f50b6bf626ca
Oumaima Chalkhoune <oumaimachalkhoune1@gmail.com>
2022-12-04 15:16:36 +0100

    added a dynamic function

--- serie.py
+++ serie.py
@@ -14,3 +14,18 @@ def serie():
 
 r=serie()
 print("la somme de serie 1! / 1 + 2! / 2 + 3! / 3 + 4! / 4 + 5! / 5 est :",r)
+
+
+#la somme de serie 1! / 1 + 2! / 2 + 3! / 3 + 4! / 4 + 5! / 5 + ....+ n! /n :
+def serie1(n):
+    e=0
+    for i in range(1,n+1):
+        e=e+(fact(i)/i)
+    return e
+
+
+n=int(input("entrez la valeur de n :"))
+e=serie1(n)
+print(" la somme de serie :",n,"est",e)
+
+
